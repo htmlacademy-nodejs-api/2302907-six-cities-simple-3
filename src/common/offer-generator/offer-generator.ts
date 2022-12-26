999923514a0a092354a84662a7dde05381046492
Lira Holm <liraholm@gmail.com>
2022-12-27 09:49:02 +1000

    1. Описаны функции - для генерации случайных чисел в указанном диапазоне. - для выбора случайных элементов массива - для выбора 1 случайного элемента массива
    2. Описан интерфейс для команды offer-generator
    3. Описаны ограничения для генерируемых данных
    4. Добавлен файл с запросом для получения данных с ресурса `/api`.

---
+++ src/common/offer-generator/offer-generator.ts
@@ -0,0 +1,19 @@
+const RESTRICTIONS = {
+  price: {
+    min: 100,
+    max: 100000,
+  },
+  rating: {
+    min: 1,
+    max: 5,
+  },
+  guestsCount: {
+    min: 1,
+    max: 10,
+  },
+  roomsCount: {
+    min: 1,
+    max: 8,
+  },
+};
+
