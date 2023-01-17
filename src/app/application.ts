e49885b22b2779e19b2c25d1d423a1fa4303ab8b
Lira Holm <liraholm@gmail.com>
2023-01-18 05:01:35 +1000

    Добавлен логгер и базовая инициализация приложения

---
+++ src/app/application.ts
@@ -0,0 +1,10 @@
+import {LoggerInterface} from '../common/logger/logger.interface.js';
+
+export default class Application {
+  constructor(private logger: LoggerInterface) {
+  }
+
+  public async init() {
+    this.logger.info('Application initialization...');
+  }
+}
