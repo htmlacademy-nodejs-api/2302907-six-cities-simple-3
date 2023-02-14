c81c2d498d8a29442ea4a674feed39a65ef040d5
Lira Holm <liraholm@gmail.com>
2023-02-14 17:24:50 +1000

    Исправлены типы и структура для моковых данных

--- src/types/mock-data.type.ts
+++ src/types/mock-data.type.ts
@@ -1,22 +1,17 @@
 import {CityType} from './city.type.js';
+import {UserType} from './user.type.js';
 
 
 export type MockDataType = {
   offers: {
     titles: string[],
     descriptions: string[],
-    cities: CityType[],
     imgPreviews: string[],
     images: string[],
     isPremium: boolean[],
     types: string[],
     goods: string[],
   },
-  users: {
-    emails: string[],
-    names: string[],
-    avatars: string[],
-    passwords: string[],
-    userTypes: string[],
-  }
+  users: UserType[],
+  cities: CityType[],
 }
