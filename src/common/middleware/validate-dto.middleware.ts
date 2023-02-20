8f3f5b056b35afb77e3def14ea7136c6766b481d
Lira Holm <liraholm@gmail.com>
2023-02-20 22:19:36 +1000

    Доработан формат ошибок валидации

--- src/common/middleware/validate-dto.middleware.ts
+++ src/common/middleware/validate-dto.middleware.ts
@@ -2,18 +2,19 @@ import {MiddlewareInterface} from '../../types/middleware.interface.js';
 import {ClassConstructor, plainToInstance} from 'class-transformer';
 import {NextFunction, Request, Response} from 'express';
 import {validate} from 'class-validator';
-import {StatusCodes} from 'http-status-codes';
+import ValidationError from '../error/validation-error.js';
+import {transformErrors} from '../../utils/common.js';
 
 export class ValidateDtoMiddleware implements MiddlewareInterface {
   constructor(private dto: ClassConstructor<object>) {}
 
-  public async execute({body}: Request, res: Response, next: NextFunction) {
+  public async execute(req: Request, _res: Response, next: NextFunction) {
+    const {body} = req;
     const dtoInstance = plainToInstance(this.dto, body);
     const errors = await validate(dtoInstance);
 
     if (errors.length > 0) {
-      res.status(StatusCodes.BAD_REQUEST).send(errors);
-      return;
+      throw new ValidationError(`Validation error: "${req.path}`, transformErrors(errors));
     }
 
     next();
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -5,6 +5,9 @@ import {LocationType} from '../types/location.type.js';
 import {ObjectType} from '../types/object.type.js';
 import {GoodsType} from '../types/goods.type.js';
 import {ClassConstructor, plainToInstance} from 'class-transformer';
+import {ValidationErrorFieldType} from '../types/validation-error-field.type.js';
+import {ValidationError} from 'class-validator';
+import {ServiceError} from '../types/service-error.enum.js';
 
 export const createOffer = (row: string): OfferType => {
   const tokens = row.replace('\n', '').split('\t');
@@ -49,8 +52,10 @@ export const createSHA256 = (line: string, salt: string) => {
 export const fillDTO = <T, V>(someDto: ClassConstructor<T>, plainObject: V) =>
   plainToInstance(someDto, plainObject, {excludeExtraneousValues: true});
 
-export const createErrorObject = (message: string) => ({
-  error: message,
+export const createErrorObject = (serviceError: ServiceError, message: string, details: ValidationErrorFieldType[] = []) => ({
+  errorType: serviceError,
+  message,
+  details: [...details]
 });
 
 export const createJWT = async (algorithm: string, jwtSecret: string, payload: object): Promise<string> =>
@@ -59,3 +64,10 @@ export const createJWT = async (algorithm: string, jwtSecret: string, payload: o
     .setIssuedAt()
     .setExpirationTime('2h')
     .sign(crypto.createSecretKey(jwtSecret, 'utf-8'));
+
+export const transformErrors = (errors: ValidationError[]): ValidationErrorFieldType[] =>
+  errors.map(({property, value, constraints}) => ({
+    property,
+    value,
+    messages: constraints ? Object.values(constraints) : []
+  }));
